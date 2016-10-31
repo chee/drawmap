dd311457f857ddb2ff12fe6f89c6fc3bfcb4c620
chee <chee@snaek.org>
2016-10-31 21:22:24 +0000

    polygons are hard

--- app/buttons.js
+++ app/buttons.js
@@ -1,6 +1,11 @@
+import {DRAW_TOOL, ERASE_TOOL} from './draw'
+
 const draw = document.getElementById('draw-button')
+const erase = document.getElementById('erase-button')
 const move = document.getElementById('move-button')
-const buttons = [draw, move]
+const canvas = document.getElementById('draw')
+
+const buttons = [draw, erase, move]
 
 function setActiveButton(event) {
   buttons.forEach(button => {
@@ -9,14 +14,20 @@ function setActiveButton(event) {
   event.target.classList.add('selected')
 }
 
+function enableDraw(tool) {
+  canvas.classList.remove('pointer-events--none')
+  canvas.setAttribute('data-tool', tool)
+}
+
+function disableDraw() {
+  canvas.classList.add('pointer-events--none')
+}
+
 buttons.forEach(button => {
   button.addEventListener('click', setActiveButton)
 })
 
-draw.addEventListener('click', event => {
-  document.getElementById('draw').classList.remove('pointer-events--none')
-})
+draw.addEventListener('click', () => enableDraw(DRAW_TOOL))
+erase.addEventListener('click', () => enableDraw(ERASE_TOOL))
 
-move.addEventListener('click', event => {
-  document.getElementById('draw').classList.add('pointer-events--none')
-})
+move.addEventListener('click', disableDraw)
--- app/draw.js
+++ app/draw.js
@@ -1,4 +1,9 @@
-import simplify from 'simplify-js'
+import simplifyjs from 'simplify-js'
+import convex from 'convexhull-js'
+import {hexToRgba} from 'hex-and-rgba'
+
+export const DRAW_TOOL = 'draw'
+export const ERASE_TOOL = 'erase'
 
 const canvas = document.getElementById('draw')
 const container = document.getElementById('map-container')
@@ -6,9 +11,75 @@ const box = container.getBoundingClientRect()
 const context = canvas.getContext('2d')
 let points = []
 const polygons = []
+const mapPolygons = []
+
+function makeMapPolygon({map, polygon, color}) {
+  const coordinates = polygon.map(point => {
+    return pixelToCoordinate(point, map)
+  })
+  const mapPolygon = new google.maps.Polygon({
+    paths: coordinates,
+    strokeColor: color,
+    strokeWeight: 2,
+    fillColor: color,
+    fillOpacity: 0.4
+  })
+  mapPolygon.setMap(map)
+  return mapPolygon
+}
+
+const tools = {}
+
+tools[DRAW_TOOL] = {
+  color: '#ff2a50',
+  up({polygon, map}) {
+    const mapPolygon = makeMapPolygon({
+      map, polygon, color: this.color
+    })
+    polygons.push(polygon)
+    mapPolygons.push(mapPolygon)
+  }
+}
+
+tools[ERASE_TOOL] = {
+  color: '#3399ff',
+  up({polygon, map}) {
+    polygons.forEach((livePolygon, index) => {
+      polygons[index] = livePolygon.union(new Polygon(polygon))
+      console.log(livePolygon.cut(new Polygon(polygon)))
+      const newPolygon = polygons[index]
+      const coordinates = newPolygon.points.map(point => {
+        return pixelToCoordinate(point, map)
+      })
+      console.log(window.coordinates = coordinates)
+      console.log(newPolygon)
+      const mapPolygon = new google.maps.Polygon({
+        paths: coordinates,
+        strokeColor: this.color,
+        strokeWeight: 2,
+        fillColor: this.color,
+        fillOpacity: 0.4
+      })
+      mapPolygon.setMap(map)
+    })
+  }
+}
 
 canvas.setAttribute('width', box.width)
 canvas.setAttribute('height', box.height)
+
+function max(array) {
+  return array.reduce((a, b) => Math.max(a, b))
+}
+
+function min(array) {
+  return array.reduce((a, b) => Math.min(a, b))
+}
+
+function simplify(points) {
+  const simplePoints = simplifyjs(points, 4, true)
+  return convex(points)
+}
 
 function addPoint(x, y) {
   points.push({
@@ -17,10 +88,11 @@ function addPoint(x, y) {
   })
 }
 
-function redraw() {
+function redraw({color}) {
+  const [r, g, b, a] = hexToRgba(`${color}66`)
   context.clearRect(0, 0, canvas.width, canvas.height)
-  context.strokeStyle = '#ff2a50'
-  context.fillStyle = 'rgba(255, 47, 74, 0.2)'
+  context.strokeStyle = color
+  context.fillStyle = `rgba(${r}, ${g}, ${b}, ${a})`
   context.lineJoin = 'round'
   context.lineWidth = 5
   context.beginPath()
@@ -42,44 +114,37 @@ function pixelToCoordinate(point, map) {
 }
 
 document.addEventListener('mapready', event => {
+  const google = window.google
   const map = event.detail
   let painting = false
 
   function down(event) {
+    const tool = tools[canvas.getAttribute('data-tool')]
     painting = true
     const x = event.offsetX || event.touches[0].pageX
     const y = event.offsetY || event.touches[0].pageY
     addPoint(x, y)
-    redraw()
+    redraw(tool)
   }
 
   function up(event) {
+    const tool = tools[canvas.getAttribute('data-tool')]
     painting = false
     if (!points.length) return
-    const simplePoints = simplify(points, 4, true)
-    const coordinates = simplePoints.map(point => {
-      return pixelToCoordinate(point, map)
-    })
-    const polygon = new google.maps.Polygon({
-      paths: coordinates,
-      strokeColor: '#ff2a50',
-      strokeWeight: 2,
-      fillColor: '#ff2a50',
-      fillOpacity: 0.4
-    })
-    polygons.push(polygon)
-    polygon.setMap(map)
+    const polygon = simplify(points)
+    tool.up({polygon, map})
     points = []
-    redraw()
+    redraw(tool)
   }
 
   function move(event) {
     event.preventDefault()
+    const tool = tools[canvas.getAttribute('data-tool')]
     const x = event.offsetX || event.touches[0].pageX
     const y = event.offsetY || event.touches[0].pageY
     if (painting) {
       addPoint(x, y)
-      redraw()
+      redraw(tool)
     }
   }
 
