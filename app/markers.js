0379f058397c107f03b89a9395915c579a6d194f
chee <chee@snaek.org>
2016-11-09 15:48:05 +0000

    load markers if they are in the selected area (and not if they are in an erased area)

---
+++ app/markers.js
@@ -0,0 +1,57 @@
+import {
+  within,
+  point as turfPoint,
+  featureCollection
+} from '@turf/turf'
+
+let markers = []
+
+document.addEventListener('search', event => {
+  markers.forEach(marker => marker.setMap(null))
+  markers = []
+  const map = event.detail
+  const geoJson = new Promise(resolve => {
+    map.data.toGeoJson(resolve)
+  })
+  const searchData = geoJson.then(getSearchData)
+
+  Promise.all([geoJson, searchData]).then(([geoJson, searchData]) => {
+    const points = featureCollection(
+      searchData.markers.map(convertMarkerDataToPoint)
+    )
+    const containedPoints = within(points, geoJson)
+
+    if (containedPoints && containedPoints.features.length) {
+      containedPoints.features
+        .map(makeMarker)
+        .forEach(marker => {
+          markers.push(marker)
+          marker.setMap(map)
+        })
+    }
+  })
+})
+
+function getSearchData(geoJson) {
+  return fetch('/markers.json', {
+    geoJson
+  }).then(data => data.json())
+}
+
+function convertMarkerDataToPoint(marker) {
+  return turfPoint([
+    Number(marker.Longitude),
+    Number(marker.Latitude)
+  ], marker)
+}
+
+function makeMarker(point) {
+  const icon = `//propertynews.com/${point.properties.Icon || '/images/sitefiles/map-icons/map-icon.png'}`
+  const [lng, lat] = point.geometry.coordinates
+
+  return new window.google.maps.Marker({
+    position: { lng, lat },
+    title: point.properties.Title,
+    icon
+  })
+}
